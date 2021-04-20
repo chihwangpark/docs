a9efd4ba5e3f7f28549b190786bae9750c68ad33
chihwngpark <82854282+chihwngpark@users.noreply.github.com>
2021-04-19 22:15:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Demo webpage!</title>
+  </head>
+  <body>
+    <h1>Demo for my HCI 285</h1>
+    <p>this is a sample page that links github to heroku for pure Awesomensss</p>
+    <h4>Chihwang Park</h4>
+  </body>
+</html>
