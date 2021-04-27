68c13b49a437de9ba837f025272d16fe57e8954d
chihwngpark <82854282+chihwngpark@users.noreply.github.com>
2021-04-27 00:51:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <title>Demo webpage!</title>
   </head>
   <body>
+    <a href="css.demo.html"> This is a link to the demo file</a>
     <h1>Demo for my HCI 285</h1>
     <p>this is a sample page that links github to heroku for pure Awesomensss</p>
     <h4>Chihwang Park</h4>
